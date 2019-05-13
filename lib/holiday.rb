80c91d431288e313ef606438aa1d331f8d6e8fcf
Natalie J <natsetc@gmail.com>
2019-05-13 09:47:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,9 +57,8 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
-    
     holiday.each do |holiday, supplies|
-      
+      puts season.capi
     end
   end
 end
