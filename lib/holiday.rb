c2ad0a4999140e76de17d224b6497b76489060b0
Natalie J <natsetc@gmail.com>
2019-05-13 09:46:33 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,9 +56,9 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash.each do 
-    |season, holiday|
-    holiday.each do
+  holiday_hash.each do |season, holiday|
+    
+    holiday.each do |holiday, supplies|
       
     end
   end
