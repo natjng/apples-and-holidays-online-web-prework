96e2e86bf679760e62a6a45fdc45e808a4c7f06a
Natalie J <natsetc@gmail.com>
2019-05-13 10:07:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     holiday.each do |holiday, supplies|
       puts "#{season.to_s.capitalize!}:"
-      puts "#{holiday.to_s.split.each.capitalize!.join(" ")}: #{holiday.split.each.capitalize!.join(" ")}"
+      puts "#{holiday.to_s.split.each.capitalize!.join(" ")}: #{supplies.split.each.capitalize!.join(" ")}"
       
       arr_h = holiday.to_s.split
       arr_h.each {|x| x.capitalize!}
