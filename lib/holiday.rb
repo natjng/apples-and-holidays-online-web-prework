c4d37b7770720bde77d7d44c2ad85cfa7cdfb453
Natalie J <natsetc@gmail.com>
2019-05-13 10:43:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,7 +77,6 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
     holiday.each do |holiday, supplies|
       holiday if supplies == "BBQ"
-        holiday_list << 
       end
     end
   end
