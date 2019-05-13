d22f166231941db893bd1601325388fe4d37f523
Natalie J <natsetc@gmail.com>
2019-05-13 10:38:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,8 +73,14 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.select {|k,h| }
+  holiday_list = []
+  holiday_hash.each do |season, holiday|
+    holiday.each do |holiday, supplies|
+      if supplies == "BBQ"
+        holiday_list = holiday
 end
+
+holiday_hash.select {|k,h| }
 clients.select{|key, hash| hash["client_id"] == "2180" }
 
 
